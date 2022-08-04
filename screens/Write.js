d13e00745e758ab88eb0883874b11038077d9567
AsanHO <gorma00@naver.com>
2022-08-04 23:00:41 +0900

    realm create issue

--- screens/Write.js
+++ screens/Write.js
@@ -1,80 +1,85 @@
-import React, { useState } from "react";
-import styled from "styled-components/native";
+import React, { useEffect, useState } from "react";
 import { Alert } from "react-native";
+import styled from "styled-components/native";
+import { useDB } from "../context";
 import colors from "../colors";
-import { Ionicons } from "@expo/vector-icons";
 
 const View = styled.View`
   background-color: ${colors.bgColor};
   flex: 1;
+  padding: 0px 30px;
 `;
 const Title = styled.Text`
-  width: 100%;
+  color: ${colors.textColor};
+  margin: 50px 0px;
   text-align: center;
-  margin: 15px 0px;
-  font-size: 25px;
-  font-weight: 600;
+  font-size: 28px;
+  font-weight: 500;
 `;
 const TextInput = styled.TextInput`
   background-color: white;
-  padding: 10px 20px;
   border-radius: 20px;
-  font-size: 20px;
+  padding: 10px 20px;
+  font-size: 18px;
   box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
 `;
 const Btn = styled.TouchableOpacity`
+  width: 100%;
+  margin-top: 20px;
   background-color: ${colors.btnColor};
   padding: 10px 20px;
-  width: 70px;
-  height: 70px;
-  justify-content: center;
-  text-align: center;
-  border-radius: 50px;
-  box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
-  position: absolute;
-  bottom: 70px;
-  right: 40px;
+  align-items: center;
+  border-radius: 20px;
   box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
 `;
 const BtnText = styled.Text`
   color: white;
-  text-align: right;
+  font-weight: 500;
+  font-size: 18px;
 `;
+
 const Emotions = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  margin: 10px 20px;
+  margin-bottom: 20px;
 `;
 const Emotion = styled.TouchableOpacity`
-  width: 50px;
-  height: 50px;
-  background-color: ${(props) => (props.selected ? "teal" : "white")};
-  justify-content: center;
-  align-items: center;
-  border-radius: 10px;
+  background-color: white;
   box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
+  padding: 10px;
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: ${(props) =>
+    props.selected ? "rgba(41, 30, 95, 1);" : "transparent"};
 `;
 const EmotionText = styled.Text`
-  font-size: 30px;
+  font-size: 24px;
 `;
-const emotions = ["😁", "🥹", "😏", "😫", "🤮"];
 
-const Write = () => {
+const emotions = ["🤯", "🥲", "🤬", "🤗", "🥰", "😊", "🤩"];
+
+const Write = ({ navigation: { goBack } }) => {
+  const realm = useDB();
   const [selectedEmotion, setEmotion] = useState(null);
-  const [text, setText] = useState("");
-  const onChangeText = (text) => setText(text);
+  const [feelings, setFeelings] = useState("");
+  const onChangeText = (text) => setFeelings(text);
   const onEmotionPress = (face) => setEmotion(face);
-  const onsubmit = () => {
-    if (text === "") {
-      return Alert.alert("글을 써주셔야 합니다!!");
-    } else if (selectedEmotion == null) {
-      return Alert.alert("감정을 선택해주셔야 합니다!!");
+  const onSubmit = () => {
+    if (feelings === "" || selectedEmotion == null) {
+      return Alert.alert("Please complete form.");
     }
+    realm.write(() => {
+      realm.create("Feeling", {
+        _id: Date.now(),
+        emotion: selectedEmotion,
+        message: feelings,
+      });
+    });
+    goBack();
   };
-  console.log(selectedEmotion);
   return (
     <View>
-      <Title>오늘 어떤 일이 있었나요??</Title>
+      <Title>How do you feel now?</Title>
       <Emotions>
         {emotions.map((emotion, index) => (
           <Emotion
@@ -88,14 +93,13 @@ const Write = () => {
       </Emotions>
       <TextInput
         returnKeyType="done"
+        onSubmitEditing={onSubmit}
         onChangeText={onChangeText}
-        value={text}
+        value={feelings}
         placeholder="Write your feelings..."
       />
-      <Btn onPress={onsubmit}>
-        <BtnText>
-          <Ionicons name="save-outline" size={30} />
-        </BtnText>
+      <Btn onPress={onSubmit}>
+        <BtnText>Save</BtnText>
       </Btn>
     </View>
   );
